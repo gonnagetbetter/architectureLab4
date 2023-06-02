243000c53d8955454a3693a770e93e1713c5f10c
gonnagetbetter <denissss906@gmail.com>
2023-06-02 23:54:07 +0300

    Added test

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,120 @@
 package main
 
-import "testing"
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
 
-func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте юніт-тест для балансувальникка.
+    . "gopkg.in/check.v1"
+
+    "github.com/jarcoal/httpmock"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+func (s *MySuite) TestScheme(c *C) {
+    *https = false
+    c.Assert(scheme(), Equals, "http")
+
+    *https = true
+    c.Assert(scheme(), Equals, "https")
+
+    *https = false
+}
+
+func (s *MySuite) TestFindBestServer(c *C) {
+    serversPool = []*Server{
+        {URL: "Server1", DataProcessed: 10, Healthy: false},
+        {URL: "Server2", DataProcessed: 20, Healthy: false},
+        {URL: "Server3", DataProcessed: 30, Healthy: false},
+    }
+    c.Assert(findBestServer(serversPool), Equals, -1)
+
+    serversPool = []*Server{
+        {URL: "Server1", DataProcessed: 10, Healthy: true},
+        {URL: "Server2", DataProcessed: 20, Healthy: true},
+        {URL: "Server3", DataProcessed: 30, Healthy: true},
+    }
+    c.Assert(findBestServer(serversPool), Equals, 0)
+
+    serversPool = []*Server{
+        {URL: "Server1", DataProcessed: 10, Healthy: false},
+        {URL: "Server2", DataProcessed: 20, Healthy: true},
+        {URL: "Server3", DataProcessed: 30, Healthy: true},
+    }
+    c.Assert(findBestServer(serversPool), Equals, 1)
+
+    serversPool = []*Server{
+        {URL: "Server1", DataProcessed: 10, Healthy: true},
+        {URL: "Server2", DataProcessed: 5, Healthy: true},
+        {URL: "Server3", DataProcessed: 30, Healthy: true},
+    }
+    c.Assert(findBestServer(serversPool), Equals, 1)
+}
+
+func (s *MySuite) TestHealth(c *C) {
+    mockURL := "http://example.com/health"
+    httpmock.RegisterResponder(http.MethodGet, mockURL, httpmock.NewStringResponder(http.StatusOK, ""))
+
+    httpmock.Activate()
+    defer httpmock.DeactivateAndReset()
+
+    server := &Server{
+        URL: "example.com",
+    }
+
+    result := health(server)
+
+    c.Assert(result, Equals, true)
+    c.Assert(server.Healthy, Equals, true)
+
+    server.Healthy = false // resetting before next test
+
+    httpmock.RegisterResponder(http.MethodGet, mockURL, httpmock.NewStringResponder(http.StatusInternalServerError, ""))
+    result2 := health(server)
+
+    c.Assert(result2, Equals, false)
+    c.Assert(server.Healthy, Equals, false)
+}
+
+func (s *MySuite) TestForward(c *C) {
+    httpmock.Activate()
+    defer httpmock.DeactivateAndReset()
+
+    httpmock.RegisterResponder("GET", "http://server1:8080/",
+        httpmock.NewStringResponder(200, "OK"))
+
+    serversPool = []*Server{
+        {URL: "server1:8080", Healthy: true, DataProcessed: 0},
+    }
+
+    req, err := http.NewRequest("GET", "/", nil)
+    c.Assert(err, IsNil)
+    rr := httptest.NewRecorder()
+    err = forward(rr, req)
+    c.Assert(err, IsNil)
+    // Check if data processed has been updated
+    c.Assert(serversPool[0].DataProcessed, Equals, int64(2))
+}
+
+func (s *MySuite) TestForwardWithUnhealthyServer(c *C) {
+    httpmock.Activate()
+    defer httpmock.DeactivateAndReset()
+
+    httpmock.RegisterResponder("GET", "http://server1:8080/",
+        httpmock.NewStringResponder(500, "Error"))
+
+    serversPool = []*Server{
+        {URL: "server1:8080", Healthy: false, DataProcessed: 0},
+    }
+
+    req, err := http.NewRequest("GET", "/", nil)
+    c.Assert(err, IsNil)
+    rr := httptest.NewRecorder()
+    err = forward(rr, req)
+    c.Assert(err, NotNil)
 }
--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,32 +1,53 @@
 package integration
 
 import (
-	"fmt"
-	"net/http"
-	"os"
-	"testing"
-	"time"
+    "fmt"
+    . "gopkg.in/check.v1"
+    "net/http"
+    "os"
+    "time"
 )
 
 const baseAddress = "http://balancer:8090"
 
 var client = http.Client{
-	Timeout: 3 * time.Second,
+    Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
-	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
-	}
+type IntegrationTestSuite struct{}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
-	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+var _ = Suite(&IntegrationTestSuite{})
+
+func (s *IntegrationTestSuite) TestGetRequest(c *C) {
+    if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+        c.Skip("Integration test is not enabled")
+    }
+
+    serverNum := 0
+    for i := 0; i < 10; i++ {
+        resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+        c.Assert(err, IsNil)
+
+        if i%3 == 0 {
+            serverNum = 1
+        } else if i%3 == 1 {
+            serverNum = 2
+        } else {
+            serverNum = 3
+        }
+        c.Assert(resp.Header.Get("lb-from"), Equals, fmt.Sprintf("server%d:8080", serverNum))
+    }
 }
 
-func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+func (s *IntegrationTestSuite) BenchmarkBalancer(c *C) {
+    if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+        c.Skip("Integration test is not enabled")
+    }
+
+    for i := 0; i < c.N; i++ {
+        _, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+        c.Assert(err, IsNil)
+    }
 }
+
+func Test(t *testing.T) { TestingT(t) }
